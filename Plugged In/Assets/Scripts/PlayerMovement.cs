d911a21cc2957b047f11565dbb1e73cf4c146c7c
AntonyScott <antonyscott1@hotmail.co.uk>
2020-12-22 23:42:57 +0000

    Commit 1

---
+++ Plugged In/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float exposedVar = 5;
+    float notExposedVar = 5;
+
+    [SerializeField]
+    float thisCanBeSeen;
+    [Range(0, 100)]
+    public float thisHasARange;
+
+    void Start()
+    {
+        Debug.Log("This is the value " + exposedVar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+        }
+    }
+}
