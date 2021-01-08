dfbfeab512a95ff4b939baed882d2c166f156f53
AntonyScott <antonyscott1@hotmail.co.uk>
2021-01-08 00:46:35 +0000

    Commit 3
    Added firing along with a basic enemy and simple win/lose states.

---
+++ Plugged In/Assets/EnemyController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float enemyHealth = 50;
+    public float damage;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void TakeDamage(float damageTaken)
+    {
+        enemyHealth -= damageTaken;
+        if(enemyHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Bullet")
+        {
+            TakeDamage(collision.gameObject.GetComponent<BulletController>().damage);
+            Destroy(collision.gameObject);
+        }
+    }
+}
--- Plugged In/Assets/Scripts/PlayerController.cs
+++ Plugged In/Assets/Scripts/PlayerController.cs
@@ -8,14 +8,23 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
 
     public float speed;
+    public float bulletSpeed;
+    public float playerHealth = 100;
+    public float curHealth;
+
+    public float myDamage = 10;
 
     float movementX;
     float movementY;
+
+    public GameObject bullet;
+    public Transform firePoint;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        curHealth = playerHealth;
     }
     void OnMove(InputValue inputValue)
     {
@@ -31,7 +40,7 @@ public class PlayerController : MonoBehaviour
 
         rb.AddForce(movement * speed);
 
-        Debug.Log(rb.velocity);
+        //Debug.Log(rb.velocity);
 
         if(rb.velocity.x > 10)
         {
@@ -41,18 +50,45 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 10);
         }
+        if (rb.velocity.x < -10)
+        {
+            rb.velocity = new Vector3(-10, rb.velocity.y, rb.velocity.z);
+        }
+        if (rb.velocity.z < -10)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -10);
+        }
     }
 
     // Update is called once per frame
 
     void OnFire(InputValue input)
     {
-        Debug.Log("FIRE!!!");
+       GameObject myBullet = Instantiate(bullet, firePoint.position, Quaternion.identity);
+        myBullet.GetComponent<Rigidbody>().AddForce(firePoint.transform.forward * bulletSpeed);
+        myBullet.GetComponent<BulletController>().damage = myDamage;
     }
     
     void OnJump(InputValue input)
     {
         rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
         Debug.Log("Jump!!!");
+    }
+    void TakeDamage(float damageTaken)
+    {
+        curHealth -= damageTaken;
+
+        if (curHealth <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+        private void OnCollisionEnter(Collision colllision)
+    {
+        if(colllision.transform.tag == "Enemy")
+        {
+            TakeDamage(10);
+            print("You are taking damage!!");
+        }
     }
 }
